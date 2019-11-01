550e019cfc257d8204a417353f6aa714f14d22b8
gaohan1994 <871418277@qq.com>
2019-11-01 15:41:23 +0800

    * 新增 首页样式

---
+++ src/component/common/card/card.common.tsx
@@ -0,0 +1,45 @@
+/*
+ * @Author: Ghan 
+ * @Date: 2019-11-01 14:06:03 
+ * @Last Modified by: Ghan
+ * @Last Modified time: 2019-11-01 14:08:52
+ */
+import Taro from '@tarojs/taro';
+import { View } from '@tarojs/components';
+import { ViewProps } from '@tarojs/components/types/View';
+import classnames from 'classnames';
+import { merge } from 'lodash';
+import './card.less';
+
+interface CardProps extends ViewProps {
+  shadow?: boolean; // 是否显示阴影
+}
+
+export class Card extends Taro.Component<CardProps> {
+
+  static externalClasses = ['card-class'];
+
+  static defaultProps = {
+    shadow: true
+  };
+
+  render () {
+    const { shadow } = this.props;
+
+    const CardViewProps: any = merge({}, this.props);
+    delete CardViewProps.shadow;
+    return (
+      <View 
+        {...CardViewProps}
+        className={classnames({
+          'theme-card': true, 
+          'common-card': true,
+          'theme-shadow': shadow,
+          'card-class': true,
+        })} 
+      >
+        {this.props.children}
+      </View>
+    );
+  }
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,42 @@
 import { ComponentClass } from 'react';
 import Taro, { Component, Config } from '@tarojs/taro';
-import { View, Text } from '@tarojs/components';
-import './home.less';
+import { View, Image, Text } from '@tarojs/components';
+import './style/home.less';
+import classnames from 'classnames';
+import { Card } from '../../component/common/card/card.common';
+
+const NavItems = [
+  {
+    image: '//net.huanmusic.com/weapp/icon_menu_order.png',
+    value: '商品管理',
+    subTitle: 'Commodity management',
+  },
+  {
+    image: '//net.huanmusic.com/weapp/icon_menu_order.png',
+    value: '会员管理',
+    subTitle: 'Member management',
+  },
+  {
+    image: '//net.huanmusic.com/weapp/icon_menu_order.png',
+    value: '收款',
+    subTitle: 'Gathering',
+  },
+  {
+    image: '//net.huanmusic.com/weapp/icon_menu_order.png',
+    value: '采购收货',
+    subTitle: 'Procurement',
+  },
+  {
+    image: '//net.huanmusic.com/weapp/icon_menu_order.png',
+    value: '采购下单',
+    subTitle: 'Purchasing order',
+  },
+  {
+    image: '//net.huanmusic.com/weapp/icon_menu_order.png',
+    value: '更多',
+    subTitle: 'Even more',
+  }
+];
 
 type PageState = {};
 type IProps = {};
@@ -25,8 +60,46 @@ class Home extends Component {
 
   render () {
     return (
-      <View>
-        <View><Text>Hello, World</Text></View>
+      <View className={classnames(['container', 'home'])}>
+        <View className="home-name">
+          <Image src="//net.huanmusic.com/weapp/icon_shop.png" className={classnames(['home-name-icon', 'home-icon'])} />
+          <Text className="home-name-text">可乐便利店</Text>
+        </View>
+        <Card card-class="home-card">
+          <View className="home-buttons">
+            <View className="home-buttons-button home-buttons-button-border">
+              <View className="normal-text">今日销售额 ></View>
+              <View className="home-money">100000.00</View>
+            </View>
+            <View className="home-buttons-button home-buttons-button-end">
+              <View className="normal-text">今日销售额 ></View>
+              <View className="home-money">100000.00</View>
+            </View>
+          </View>
+        </Card>
+        <Card card-class="home-order">
+          <Image src="//net.huanmusic.com/weapp/icon_home_bill.png" className="home-order-icon" />
+          <Text className="home-order-text" >开单</Text>
+        </Card>
+        <View className="home-bar">
+          {
+            NavItems.map((item, index) => {
+              return (
+                <Card 
+                  key={item.value}
+                  shadow={false} 
+                  card-class={`home-bar-card ${(index + 1) % 3 !== 0 ? 'home-bar-card-right' : ''}`} 
+                >
+                  <View className="home-bar-card-content">
+                    <Image className="home-icon" src={item.image} />
+                    <Text className="normal-text">{item.value}</Text>
+                    <Text className="home-small-text">{item.subTitle}</Text>
+                  </View>
+                </Card>
+              );
+            })
+          }
+        </View>
       </View>
     );
   }
